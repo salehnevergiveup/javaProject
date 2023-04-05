e09f6bb0e2810da4e341ec6c9255ce025d9852dd
salehnevergiveup <salehaldhaheri09@gmail.com>
2023-04-05 21:30:35 +0800

    this java

--- test-github/src/main/java/com/mycompany/test/github/TestGithub.java
+++ test-github/src/main/java/com/mycompany/test/github/TestGithub.java
@@ -4,13 +4,49 @@
 
 package com.mycompany.test.github;
 
+import java.util.Scanner;
+
 /**
  *
  * @author User
  */
-public class TestGithub {
 
+
+public class TestGithub{
+     
+    public static  int checkint() {
+        
+      int x; 
+      Scanner input = new Scanner(System.in); 
+      
+      try { 
+            x = input.nextInt(); 
+          
+      }catch(Exception e) { 
+          System.out.println("only numbers are allowed !!!!");
+            return checkint();
+      }
+       return x; 
+     }
+     
+    
     public static void main(String[] args) {
         System.out.println("Hello World!");
+         Scanner input = new Scanner(System.in); 
+         
+         int arr[] = new int[5];
+         int i =0;
+         while(i < 5) { 
+             System.out.println("please enter the  number: ");
+              
+            
+              
+             arr[i] =  checkint();
+              
+             i++; 
+             
+         }
+         
+         
     }
 }
