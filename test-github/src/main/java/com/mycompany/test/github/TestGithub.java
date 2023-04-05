acf0e437d4904b1a55fd22de28ea7a59e3fb9e9b
salehnevergiveup <salehaldhaheri09@gmail.com>
2023-04-05 20:38:51 +0800

    this is java project

---
+++ test-github/src/main/java/com/mycompany/test/github/TestGithub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.test.github;
+
+/**
+ *
+ * @author User
+ */
+public class TestGithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
